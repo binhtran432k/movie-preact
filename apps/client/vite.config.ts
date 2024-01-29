f400bfe594329f7ea1e74f2c682faee0a5537970
Binh Duc Tran <binhtran432k@gmail.com>
2024-01-29 11:16:39 +0700

    build(client): setup million

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,7 +1,8 @@
+import million from 'million/compiler';
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [million.vite({ auto: true }), preact()],
 })
