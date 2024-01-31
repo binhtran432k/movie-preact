84bed29f0c6e40fdf152ca5d9c1e17ac9bf3b23d
Binh Duc Tran <binhtran432k@gmail.com>
2024-01-31 20:39:23 +0700

    feat(wouter): add basic navigates

--- apps/client/src/app.tsx
+++ apps/client/src/app.tsx
@@ -1,7 +1,14 @@
+import { BROWSE_PATH, SEARCH_PATH } from "@/constants/paths";
+import { useHashLocation } from "@/hooks/use-hash-location";
+import Browse from "@/pages/browse/browse";
+import Search from "@/pages/search/search";
+import { Route, Router } from "wouter";
+
 export function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello, World!</h1>
-    </>
+    <Router hook={useHashLocation}>
+      <Route path={BROWSE_PATH} component={Browse} />
+      <Route path={SEARCH_PATH} component={Search} />
+    </Router>
   );
 }
---
+++ apps/client/src/constants/paths.ts
@@ -0,0 +1,2 @@
+export const BROWSE_PATH = "/";
+export const SEARCH_PATH = "/search";
