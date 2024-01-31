d254917e1206082516180e83d4e03a0cc4eb6c33
Binh Duc Tran <binhtran432k@gmail.com>
2024-01-31 22:50:04 +0700

    refactor(import): change pages import rule

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,7 @@
 import { BROWSE_PATH, SEARCH_PATH } from "@/constants/paths";
 import { useHashLocation } from "@/hooks/use-hash-location";
-import Browse from "@/pages/browse/browse";
-import Search from "@/pages/search/search";
+import Browse from "@/pages/browse/Browse";
+import Search from "@/pages/search/Search";
 import { Route, Router } from "wouter";
 
 export function App() {
