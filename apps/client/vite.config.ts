1743941866cf9b450eac8790708f1de80fc8a1f3
Binh Duc Tran <binhtran432k@gmail.com>
2024-01-31 19:26:20 +0700

    refactor(import): use root alias pattern

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,3 +1,4 @@
+import { URL, fileURLToPath } from "url";
 import preact from "@preact/preset-vite";
 import million from "million/compiler";
 import { defineConfig } from "vite";
@@ -5,4 +6,12 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [million.vite({ auto: true }), preact()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
 });
