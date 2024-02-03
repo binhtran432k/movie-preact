7da9461319cbdc14a5ae703ae97618e34cabf58e
Binh Duc Tran <binhtran432k@gmail.com>
2024-02-03 16:51:29 +0700

    refactor(style/navbar): add container wrapper

--- apps/client/src/components/NavBar.tsx
+++ apps/client/src/components/NavBar.tsx
@@ -14,17 +14,19 @@ const NavBar = memo(() => {
       className={clsx(
         // Change background to black when scroll top bigger than 100px
         scrollTop > 100 && "bg-black",
-        "fixed px-3 py-1 w-full transition-colors duration-500 flex justify-between",
+        "fixed w-full transition-colors duration-500",
       )}
     >
-      <h1 className="font-bold text-lg text-red-500 hover:text-red-400">
-        <Link href={PATH_BROWSE} id="logo">
-          Movie App
+      <div className="container mx-auto px-3 py-1 flex justify-between">
+        <h1 className="font-bold text-lg text-red-500 hover:text-red-400">
+          <Link href={PATH_BROWSE} id="logo">
+            Movie App
+          </Link>
+        </h1>
+        <Link href={PATH_SEARCH}>
+          <MagnifyingGlassIcon className="h-6 text-gray-100 hover:text-white" />
         </Link>
-      </h1>
-      <Link href={PATH_SEARCH}>
-        <MagnifyingGlassIcon className="h-6 text-gray-300 hover:text-gray-200" />
-      </Link>
+      </div>
     </div>
   );
 });
