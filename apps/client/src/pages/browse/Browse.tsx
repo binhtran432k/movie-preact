9fe6075efaabfec0eb0f02e12f8c789a169ba4fb
Binh Duc Tran <binhtran432k@gmail.com>
2024-02-20 21:37:20 +0700

    feat(page/browse): add more movie types

--- apps/client/src/pages/browse/Browse.tsx
+++ apps/client/src/pages/browse/Browse.tsx
@@ -2,38 +2,59 @@ import BackdropList from "@/components/BackdropList";
 import Banner from "@/components/Banner";
 import Navbar from "@/components/NavBar";
 import PosterList from "@/components/PosterList";
-import { getDiscoverMovies, getTrendingMovies } from "@/services/browseService";
-import { Movie } from "@/utils/definitions";
-import { useEffect, useMemo, useState } from "preact/hooks";
+import {
+  fetchActionMovies,
+  fetchComedyMovies,
+  fetchDiscoverMovies,
+  fetchDocumentaries,
+  fetchHorrorMovies,
+  fetchRomanceMovies,
+  fetchTopRated,
+  fetchTrendingMovies,
+} from "@/services/browseService";
+import { Movie, Page } from "@/utils/definitions";
+import { AxiosResponse } from "axios";
+import { StateUpdater, useEffect, useMemo, useState } from "preact/hooks";
 
 function getRandomIdx(n: number) {
   return Math.floor(Math.random() * n);
 }
 
+function fetchMovie(
+  fetchFunc: () => Promise<AxiosResponse<Page<Movie>>>,
+  setFunc: StateUpdater<Movie[]>,
+) {
+  fetchFunc()
+    .then((res) => {
+      setFunc(res.data.results);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
 export default function Browse() {
   const [movies, setMovies] = useState<Movie[]>([]);
-  const [trendingMovies, setTrendingMovies] = useState<Movie[]>([]);
+  const [trending, setTrendingMovies] = useState<Movie[]>([]);
+  const [topRated, setTopRated] = useState<Movie[]>([]);
+  const [actionMovies, setActionMovies] = useState<Movie[]>([]);
+  const [comedyMovies, setComedyMovies] = useState<Movie[]>([]);
+  const [horrorMovies, setHorrorMovies] = useState<Movie[]>([]);
+  const [romanceMovies, setRomanceMovies] = useState<Movie[]>([]);
+  const [documentaries, setDocumentaries] = useState<Movie[]>([]);
   const [idx, setIdx] = useState(-1);
   const randomIdx = useMemo(() => getRandomIdx(movies.length), [movies.length]);
 
   useEffect(() => {
-    // Get Discovery Movies and store it to state
-    getDiscoverMovies()
-      .then((res) => {
-        setMovies(res.data.results);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-
-    // Get Trending Movies and store it to state
-    getTrendingMovies()
-      .then((res) => {
-        setTrendingMovies(res.data.results);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    // Get Movies and store it to state
+    fetchMovie(fetchDiscoverMovies, setMovies);
+    fetchMovie(fetchTrendingMovies, setTrendingMovies);
+    fetchMovie(fetchTopRated, setTopRated);
+    fetchMovie(fetchActionMovies, setActionMovies);
+    fetchMovie(fetchComedyMovies, setComedyMovies);
+    fetchMovie(fetchHorrorMovies, setHorrorMovies);
+    fetchMovie(fetchRomanceMovies, setRomanceMovies);
+    fetchMovie(fetchDocumentaries, setDocumentaries);
   }, []);
 
   const movie = movies[idx !== -1 ? idx : randomIdx];
@@ -47,7 +68,13 @@ export default function Browse() {
         overview={movie?.overview}
       />
       <PosterList movies={movies} idx={idx} setIdx={setIdx} />
-      <BackdropList title="Trending" movies={trendingMovies} />
+      <BackdropList title="Trending" movies={trending} />
+      <BackdropList title="Top Rated" movies={topRated} />
+      <BackdropList title="Action Movies" movies={actionMovies} />
+      <BackdropList title="Comedy Movies" movies={comedyMovies} />
+      <BackdropList title="Horror Movies" movies={horrorMovies} />
+      <BackdropList title="Romance Movies" movies={romanceMovies} />
+      <BackdropList title="Documentaries" movies={documentaries} />
     </div>
   );
 }
