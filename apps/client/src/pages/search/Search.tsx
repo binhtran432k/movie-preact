f7e0eed17eb0233cce3e7f5678517e0faaf1d371
Binh Duc Tran <binhtran432k@gmail.com>
2024-02-23 16:15:00 +0700

    feat(page): implement search and show result

--- apps/client/src/pages/search/Search.tsx
+++ apps/client/src/pages/search/Search.tsx
@@ -1,3 +1,23 @@
+import NavBar from "@/components/NavBar";
+import ResultList from "@/components/ResultList";
+import SearchForm from "@/components/SearchForm";
+import { MovieProvider } from "@/store/MovieProvider";
+import { SearchProvider } from "@/store/SearchProvider";
+
 export default function Search() {
-  return <>Search</>;
+  return (
+    <div className="mb-16">
+      <NavBar />
+      <SearchProvider>
+        <div className="container mx-auto p-4 mt-20">
+          <div className="max-w-[30rem] m-auto mb-10">
+            <SearchForm />
+          </div>
+          <MovieProvider>
+            <ResultList />
+          </MovieProvider>
+        </div>
+      </SearchProvider>
+    </div>
+  );
 }
